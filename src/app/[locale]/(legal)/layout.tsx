8e3c8c275d0debbfa77999b0e5ac55c236fb9e3c
itsosmx <husseinosama179@gmail.com>
2026-05-05 21:55:43 +0300

    Remove 'prose-invert' class from legal layout component for improved styling consistency

--- src/app/[locale]/(legal)/layout.tsx
+++ src/app/[locale]/(legal)/layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function layout({ children }: { children: React.ReactNode }) {
-  return <div className="prose prose-invert container mx-auto max-w-7xl my-20">{children}</div>;
+  return <div className="prose container mx-auto max-w-7xl my-20">{children}</div>;
 }
